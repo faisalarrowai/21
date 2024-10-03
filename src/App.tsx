84c3b47728f7c78c0eddb4f3edb7ab30baba4ed8
faisal004 <faisalhusain1320@gmail.com>
2024-10-03 10:33:27 +0530

    Commit changes before publishing to remote

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
+import React from 'react';
+import Clock from './components/Clock';
 
 function App() {
-
   return (
     <div className='flex items-center justify-center flex-col'>
-    CodeBolt Template
+      <h1>CodeBolt Template</h1>
+      <Clock />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const Clock: React.FC = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  const hours = String(time.getHours()).padStart(2, '0');
+  const minutes = String(time.getMinutes()).padStart(2, '0');
+  const seconds = String(time.getSeconds()).padStart(2, '0');
+
+  return (
+    <div className="clock">
+      <span>{hours}:{minutes}:{seconds}</span>
+    </div>
+  );
+};
+
+export default Clock;
